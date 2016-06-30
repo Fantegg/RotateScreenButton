31277ebc4db458ab39d7d5a388f4257561055ae3
Mikhail <fangegg@live.com>
2016-06-30 11:53:21 +0000

    Add files via upload
    Inwork console app

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO.Ports;
+using System.Windows.Forms;
+
+namespace Rotate_And_Read
+{
+    class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+
+
+            SerialPort mySerialPort = new SerialPort("COM3");
+
+            mySerialPort.BaudRate = 9600;
+            mySerialPort.Parity = Parity.None;
+            mySerialPort.StopBits = StopBits.One;
+            mySerialPort.DataBits = 8;
+            mySerialPort.Handshake = Handshake.None;
+            mySerialPort.RtsEnable = true;
+            mySerialPort.DataReceived += new SerialDataReceivedEventHandler(DataReceivedHandler);
+            mySerialPort.Open();
+            Console.WriteLine("Press any key to continue...");
+            Console.WriteLine();
+            Console.ReadKey();
+            mySerialPort.Close();
+        }
+        private static void DataReceivedHandler(
+                        object sender,
+                        SerialDataReceivedEventArgs e)
+        {
+            SerialPort sp = (SerialPort)sender;
+            string indata = sp.ReadExisting();
+            if (indata.Equals("pressed")) Console.WriteLine("Data Received:", indata);
+             
+        }
+    }
+
+}
