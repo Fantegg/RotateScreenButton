3e53870e9f640e8dfb6f405760531e8dd9238110
Mikhail <fangegg@live.com>
2016-06-30 11:51:05 +0000

    Add files via upload
    Added Arduino sketch

---
+++ Button.ino
@@ -0,0 +1,23 @@
+// set pin numbers:
+const int buttonPin = 7;     // the number of the pushbutton pin
+
+// variables will change:
+int buttonState = 0;         // variable for reading the pushbutton status
+int i = 0;
+void setup() {
+  // initialize the pushbutton pin as an input:
+  pinMode(buttonPin, INPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  // read the state of the pushbutton value:
+  buttonState = digitalRead(buttonPin);
+  // check if the pushbutton is pressed.
+  // if it is, the buttonState is HIGH:
+  if (buttonState == HIGH) {
+    Serial.println ("pressed");
+  delay (300);
+  }
+}
+
